281045b1db4c6f0edd4683751bbf2bb8198a7b38
Pascal Wilzki <pascal.wilzki@gmail.com>
2025-02-05 19:51:08 +0100

    update

--- spendings_sankey.py
+++ spendings_sankey.py
@@ -3,9 +3,9 @@ import pandas as pd
 import plotly.graph_objects as go
 
 
-#%% READ DATA
-data_path = r"C:\Users\pasca\Documents\FastBudget\CSV_12_19__18_01_02.csv"
-data_path = r"G:\Meine Ablage\Backups\Fast budget\Fast Budget 01-24.csv"
+data_path = r"fastbudget.csv"
+YEAR = 2022
+
 data = pd.read_csv(data_path, delimiter=";", 
                    header=1, index_col="Id", 
                    parse_dates=[5], 
@@ -13,50 +13,55 @@ data = pd.read_csv(data_path, delimiter=";",
                    keep_default_na=False,
                    )
 
-#%% MAIN CATEGORIES
-categories = {  "Einkaufen": ["Lebensmittel", "Baumarkt", "Technik", "Verbrauchsartikel", "Geschenke", "Kleidung", "Essen arbeit", "Supplements", "Einkaufen"],
-                "Freizeit": ["Bar Alkohol", "Ausflug", "Kultur", "Aktivität", "Sport", "Restaurant", "PC", "Urlaub", "Software" ],
-                "Transport": ["Tanken", "OPVN", "Auto", "Reparatur", "Motorrad"],
+
+categories = {  "Einkaufen": ["Lebensmittel", "Baumarkt", "Technik", "Verbrauchsartikel", "Geschenke", "Kleidung", "Essen Arbeit", "Supplements", "Einkaufen"],
+                "Freizeit": ["Bar Alkohol", "Ausflug", "Kultur", "Aktivität", "Sport", "Restaurant", "PC", "Urlaub", "Software", "Eissport"],
+                "Transport": ["Tanken", "OPVN", "Auto", "Reparatur", "Motorrad", "Motorrad Tanken"],
                 "Wohnung": ["Wohnung", "Miete", "Strom", "Internet", "GEZ"],                
-                "Abos": ["Handyvertrag", "Netflix", "Spotify", "Lastpass", "Headspace", "Laufende Kosten"],
-                "Sonstiges": ["Sonstiges", "Ausgleich", "Friseur", "Ausgelegt"],               
+                "Abos": ["Handyvertrag", "Netflix", "Spotify", "Lastpass", "Headspace", "Laufende Kosten", "Boxen"], 
+                "Sonstiges": ["Ausgleich", "Friseur", "Ausgelegt", "sonstige Ausgaben", "Spenden"],               
                 "Sparen": ["Sparen", "Vanguard all world"], 
                 "Versicherung": ["Krankenversicherung", "Versicherung"],
                 "Archiv": ["Uni"]
                 }
 
+data = data[data["Date"] >= pd.Timestamp(YEAR,1,1)]
+data = data[data["Date"] <= pd.Timestamp(YEAR,12,31)]
+data.to_clipboard(sep=";")
 
-#%%
-year = 2022
-data = data[data["Fecha"] >= pd.Timestamp(year,1,1)]
-data = data[data["Fecha"] <= pd.Timestamp(year,12,31)]
+earnings = data[data["Value"] > 0][["Category", "Date", "Value", "Notes"]]
+earnings_categorized = earnings.groupby("Category").sum("Value")
+earnings_categorized = earnings_categorized[earnings_categorized.index != "Transfer between accounts"]
 
-earnings = data[data["Valor"] > 0][["Categoría", "Fecha", "Valor", "Notas"]]
-earnings_categorized = earnings.groupby("Categoría").sum("Valor")
+spendings = data[data["Value"] < 0][["Category", "Date", "Value", "Notes"]]
+spendings_categorized = spendings.groupby("Category").sum("Value").abs()
+spendings_categorized = spendings_categorized[spendings_categorized.index != "Transfer between accounts"]
 
-spendings = data[data["Valor"] < 0][["Categoría", "Fecha", "Valor", "Notas"]]
-spendings_categorized = spendings.groupby("Categoría").sum("Valor").abs()
+# Remove subcategories in categories that have no entries in earnings_categorized
+for main_category, subcategories in categories.items():
+    categories[main_category] = [sub for sub in subcategories if sub in spendings_categorized.index]
+categories = {k: v for k, v in categories.items() if v}
 
-if year == 2022:
+# sum up common categories from dept payback
+common_categories = set(earnings_categorized.index).intersection(spendings_categorized.index)
+for category in common_categories:
+    spendings_categorized.loc[category] -= earnings_categorized.loc[category]
+    earnings_categorized = earnings_categorized.drop(category)
+
+if YEAR == 2022:
     earnings_categorized.loc["Schulden"] = 900
     spendings_categorized.loc["Rent"] = 4600
     earnings_categorized = earnings_categorized.drop("Miete")
    
-
-
-
-spendings_categorized.loc["Sparen"] = earnings_categorized.sum()-spendings_categorized.sum()
-# %%
-
-
-
+#spendings_categorized.loc["Sparen"] = earnings_categorized.sum()-spendings_categorized.sum()
 
 flat_cats = [item for sublist in categories.values() for item in sublist]
+flat_cats = [item for item in flat_cats if item in spendings_categorized.index]
 remaining = [item for item in spendings_categorized.index if item not in flat_cats]
 
 # LABEL
 label_list = earnings_categorized.index.tolist() + ["Gesamt"]
-label_list += categories.keys()
+label_list += [key for key, vals in categories.items() if any(val in spendings_categorized.index for val in vals)]
 label_list += remaining
 label_list += flat_cats
 
@@ -66,8 +71,10 @@ cat_L = len(categories)
 rem_L = len(remaining)
 flat_L = len(flat_cats)
 
+# EARNINGS
 source = list(range(0,ear_L))
-source += [ear_L]*(cat_L+rem_L)
+
+source += [ear_L]*(cat_L+rem_L) # Gesamt as second column
 inx = ear_L + 1
 for category, vals in categories.items():
     source += [inx]*len(vals)
@@ -78,17 +85,18 @@ target = [ear_L]*ear_L
 target += list(range(ear_L+1, ear_L + 1 + cat_L + rem_L + flat_L))
 
 #COUNT
-count = earnings_categorized.Valor.tolist()
+count = earnings_categorized.Value.tolist()
 for category, vals in categories.items():
-    count.append(spendings_categorized.loc[vals].Valor.sum())
-count += spendings_categorized.loc[remaining].Valor.tolist()
-count += spendings_categorized.loc[flat_cats].Valor.tolist()
+    count.append(spendings_categorized.loc[vals].Value.sum())
+count += spendings_categorized.loc[remaining].Value.tolist()
+count += spendings_categorized.loc[flat_cats].Value.tolist()
 
 offset = 0
 for i , val in enumerate(count):
     if i == ear_L:
         offset = 1
     label_list[i+offset] = label_list[i+offset] +f" [{count[i]:.0f} €]"
+
 
 
 # %%
